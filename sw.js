1197a11021d9507e5ad5d330dbfae9c3fd2a53f2
vilmusenahojuha-stack <vilmusenahojuha@gmail.com>
2026-08-21 20:23:29 +0300

    Bump cache for V2.1 desktop polish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cheer-music-studio-v2';
+const CACHE='cheer-music-studio-v2.1';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
